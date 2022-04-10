313cb337f5126e7c8f2aff7dfb0c7bc18b090ecb
Soneye Sina <advancoplanet@gmail.com>
2022-04-10 09:49:20 +0100

    factories

---
+++ database/factories/StoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Story>
+ */
+class StoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => $this->faker->numberBetween(0,2), //0: draft, 1: published, 2: disapproved
+            'admin_id' => $this->faker->numberBetween(1,10)
+        ];
+    }
+}
